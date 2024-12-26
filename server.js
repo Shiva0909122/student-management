a663b3c4c72545bf342ed5f251fe9cbae9785f71
SHIVARTH DRONACHARY <130677924+Shiva0909122@users.noreply.github.com>
2024-12-26 22:57:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const MONGO_URI = process.env.MONGO_URI;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
